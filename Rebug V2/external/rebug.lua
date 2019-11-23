f5676bcbec28025388527736bc7137b95c46bda0
Void <54481654+Void09@users.noreply.github.com>
2019-11-23 01:36:15 +0000

    Update rebug.lua

--- Rebug V2/external/rebug.lua
+++ Rebug V2/external/rebug.lua
@@ -13,11 +13,18 @@
 
 Backdoor controller created by Project Rebug. The code for this script
 has been made organised for better understanding.
+---------------------------------------------------------
+// Coded By ProjectRebug Login Release Source
+// https://github.com/Void09/Login-Release
+// Note to the person using this, removing this
+// text is in violation of the licence you agreed
+// to by downloading.
+// Copyright © ProjectRebug 2019
+// Licenced under © GNU licence here
+// https://github.com/ProjectRebug/RebugV2-Release/blob/master/LICENSE
+---------------------------------------------------------
 
 Project Rebug: 2016-2019
-
-Licenced Under: GPL-3.0
-
 =======================================================================*/
 
 // Code for the script is on the link
